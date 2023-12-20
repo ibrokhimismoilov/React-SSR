2a867e7b0c00d54665c55984dc31ee4049053582
ibrokhim <mr017programmer@gmail.com>
2023-12-20 21:04:15 +0500

    Pravki

---
+++ scripts/start.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const Hapi = require('@hapi/hapi');
+
+const port = process.env.PORT || 3000;
+
+const FILES = /\.(js|js.map|woff|woff2|svg|bmp|jpg|jpeg|gif|png|ico|css)(\?v=\d+\.\d+\.\d+)?$/;
+
+const init = async () => {
+  const server = Hapi.server({
+    port,
+  });
+
+  await server.register(require('@hapi/inert'));
+
+  server.route({
+    method: 'GET',
+    path: '/{path*}',
+    handler: (request, h) => {
+      if (FILES.test(request.path)) {
+        return h.file(path.join(process.cwd(), 'dist', request.path));
+      }
+
+      return h.file(path.join(process.cwd(), 'dist', 'index.html'));
+    }
+  })
+
+  await server.start();
+  console.log('Server running on %s', server.info.uri);
+}
+
+process.on('unhandledRejection', (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
